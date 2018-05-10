0be840965bdaf7f73c43f5835a0f71e88337dd0e
nekocode <nekocode.cn@gmail.com>
2018-05-10 23:31:15 +0800

    Optimize

--- example/src/main/java/cn/nekocode/jarfilter/example/MainActivity.java
+++ example/src/main/java/cn/nekocode/jarfilter/example/MainActivity.java
@@ -25,5 +25,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new StringBuilder();
+        new StringBuilder();
+        new StringBuilder();
+        new StringBuilder();
     }
 }
